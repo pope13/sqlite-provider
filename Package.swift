d3416d28d86baa2895475ba9467491a24a53a01f
pope13 <pope13@gmail.com>
2016-12-04 12:29:27 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "VaporSQLite",
     dependencies: [
-   		.Package(url: "https://github.com/vapor/sqlite-driver.git", majorVersion: 1),
+   		.Package(url: "https://github.com/pope13/sqlite-driver", majorVersion: 1),
    		.Package(url: "https://github.com/vapor/vapor.git", versions: Version(1,1,0)..<Version(2,0,0)),
     ]
 )
